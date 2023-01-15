294c7316d8495c154ef6f64990917423dbe46f7e
Antonin Stefanutti <antonin@stefanutti.fr>
2023-01-15 17:05:49 +0100

    Add XCB lease support

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC=gcc
 CFLAGS=-c -g -Wall -O3 -Winvalid-pch -Wextra -std=gnu99 -fdiagnostics-color=always -pipe -pthread -I/usr/include/libdrm
 LDFLAGS=-Wl,--as-needed -Wl,--no-undefined
-LDLIBS=-lGLESv2 -lEGL -ldrm -lgbm
-SOURCES=common.c drm-atomic.c drm-common.c drm-legacy.c glsl.c perfcntrs.c shadertoy.c
+LDLIBS=-lGLESv2 -lEGL -ldrm -lgbm -lxcb-randr -lxcb
+SOURCES=common.c drm-atomic.c drm-common.c drm-legacy.c glsl.c lease.c perfcntrs.c shadertoy.c
 OBJECTS=$(SOURCES:%.c=%.o)
 EXECUTABLE=glsl
 
---
+++ lease.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2012 Arvin Schnell <arvin.schnell@gmail.com>
+ * Copyright (c) 2012 Rob Clark <rob@ti.com>
+ * Copyright (c) 2020 Antonin Stefanutti <antonin.stefanutti@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sub license,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef _LEASE_H
+#define _LEASE_H
+
+#ifndef XCB_LEASE
+#if __has_include(<xcb/xcb.h>) && __has_include(<xcb/randr.h>)
+#define XCB_LEASE 1
+#endif
+#endif
+
+#if XCB_LEASE
+
+#include <xcb/xcb.h>
+#include <xcb/randr.h>
+
+int xcb_lease(xcb_connection_t *connection, int *screen);
+
+#endif
+
+#endif /* _LEASE_H */
